273e9b8da296e084022a1bb23cd117aebf24c1fa
[LoLoo03] <[bloren641@gmail.com]>
2025-04-15 13:59:54 -0300

    Parte [3.3] Levantar un error 404

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from .models import Question
 
@@ -11,3 +11,6 @@ def index(request):
 	}
 	return render(request, "polls/index.html", context)
 
+def detail(request, question_id):
+	question = get_object_or_404(Question, pk=question_id)
+	return render(request, "polls/detail.html", {"question": question})
